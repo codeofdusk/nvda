ea851f73835858ae7d85f0afc932294e38dd824f
Bill Dengler <codeofdusk@gmail.com>
2020-04-08 19:53:29 -0400

    Revert "UI Automation in Windows Console: revert for 2019.3 (#10684)"
    This reverts commit 82eeff0e20970e6cb8465cd424060decf57c16ee.

--- source/NVDAObjects/UIA/__init__.py
+++ source/NVDAObjects/UIA/__init__.py
@@ -27,6 +27,7 @@ import api
 import textInfos
 from logHandler import log
 from UIAUtils import *
+from UIAUtils import shouldUseUIAConsole
 from NVDAObjects.window import Window
 from NVDAObjects import NVDAObjectTextInfo, InvalidNVDAObject
 from NVDAObjects.behaviors import (
@@ -945,7 +946,7 @@ class UIA(Window):
 		# Support Windows Console's UIA interface
 		if (
 			self.windowClassName == "ConsoleWindowClass"
-			and config.conf['UIA']['winConsoleImplementation'] == "UIA"
+			and shouldUseUIAConsole()
 		):
 			from . import winConsoleUIA
 			winConsoleUIA.findExtraOverlayClasses(self, clsList)
